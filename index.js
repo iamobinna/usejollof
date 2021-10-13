57f07583837a99e4ce2ca026af058ad7358119b9
shahfahadl <shahfahadiffi@gmail.com>
2021-10-13 21:24:47 +0500

    m

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path'); 
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname ,'/client/build')));
  app.get('*', (req, res) => {
