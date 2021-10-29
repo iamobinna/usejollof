4543f34aa6f684a51dac4743afe61590bef45756
shahfahadl <shahfahadiffi@gmail.com>
2021-10-29 21:22:25 +0500

    urls heroku

--- client/src/urls/index.js
+++ client/src/urls/index.js
@@ -1,4 +1,4 @@
-export const URL = 'https://usejoll.herokuapp.com/';
+export const URL = '';
 // export const URL = 'http://localhost:5000';
 // export const CLIENT_URL = 'http://localhost:3000'
-export const CLIENT_URL = 'https://usejoll.herokuapp.com/'
+export const CLIENT_URL = ''
