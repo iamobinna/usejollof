3fabec9b0cb0292c1e6dcdfcb7596e46e466a970
shahfahadl <shahfahadiffi@gmail.com>
2021-11-18 12:48:21 +0500

    urls updated

--- index.js
+++ index.js
@@ -46,16 +46,17 @@ app.use('/deliveryboy', deliveryBoyRouter);
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.MONGO_DB_URI;
 
+
+app.use(express.static(path.join(__dirname ,'/pwa/build')));
+app.get('/mobile', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pwa/build/index.html'))
+});
+
 app.use(express.static(path.join(__dirname ,'/client/build')));
  app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname + '/client/build/index.html'))
  });
-
-app.use(express.static(path.join(__dirname ,'/pwa/build')));
- app.get('mobile', (req, res) => {
-     res.sendFile(path.join(__dirname + '/pwa/build/index.html'))
- });
-
+ 
  mongoose.connect(CONNECTION_URL)
  .then(() => server.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
  .catch((error) => console.log(`${error} did not connect`));
