821ec24dac6e4a21f238eadafc5eca7a21a8b24f
shahfahadl <shahfahadiffi@gmail.com>
2021-12-16 21:50:00 +0500

    url resolved

--- client/src/urls/index.js
+++ client/src/urls/index.js
@@ -1,4 +1,4 @@
-// export const URL = '';
-export const URL = 'http://localhost:5000';
+export const URL = '';
+// export const URL = 'http://localhost:5000';
 // export const CLIENT_URL = 'http://localhost:3000'
 export const CLIENT_URL = ''
